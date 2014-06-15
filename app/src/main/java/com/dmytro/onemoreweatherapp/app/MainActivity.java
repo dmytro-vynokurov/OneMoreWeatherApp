09e3b2c9bdab399aae830fe21a10d3a51129dfcb
dvinokurov <dvinokurov777@gmail.com>
2014-06-15 04:42:54 +0300

    minor fix

--- app/src/main/java/com/dmytro/onemoreweatherapp/app/MainActivity.java
+++ app/src/main/java/com/dmytro/onemoreweatherapp/app/MainActivity.java
@@ -60,10 +60,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     
